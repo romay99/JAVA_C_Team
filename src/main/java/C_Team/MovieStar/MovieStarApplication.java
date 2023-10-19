a0b1ec4f44b5dcbdb056b5c08f54cab423f3b43e
romay <i_romay@naver.com>
2023-10-19 13:56:36 +0900

    23.10.19 수빈 초기설정커밋

---
+++ src/main/java/C_Team/MovieStar/MovieStarApplication.java
@@ -0,0 +1,13 @@
+package C_Team.MovieStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/C_Team/MovieStar/MovieStarApplicationTests.java
@@ -0,0 +1,13 @@
+package C_Team.MovieStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
